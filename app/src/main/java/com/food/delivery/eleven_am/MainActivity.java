929b24aa10e157f5cf9673f4bbe753beaa28f900
Abhinendra <abhinendragaurav@gmail.com>
2020-10-13 20:17:58 +0530

    New repo

---
+++ app/src/main/java/com/food/delivery/eleven_am/MainActivity.java
@@ -0,0 +1,14 @@
+package com.food.delivery.eleven_am;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
